54b2006b28fcee1f07f3b3612611c5f3cfa84305
argenaden <argen.adenov@emoldino.com>
2021-02-08 01:04:14 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app"
+    @focusout="isOutside"
+    tabindex="0"
+    style="outline:none;"
+  >
+    <div class="gallery">
+      <div
+        v-for="(img, idx) in imgs"
+        :key="idx"
+        class="pic"
+        @click="() => show(idx)"
+      >
+        <img :src="img.src ? img.src : img" style="width: 250px; height: 250px;">
+      </div>
+    </div>
+
+    <vue-easy-lightbox
+      :visible="visible"
+      :index="index"
+      :imgs="imgs"
+      @hide="visible = false"
+    />
+  </div>
+</template>
+
+<script>
+  import VueEasyLightbox from 'vue-easy-lightbox'
+  import say from 'sweetalert';
+
+  export default {
+    name: 'App',
+    components: {
+      VueEasyLightbox
+    },
+    data() {
+      return {
+        imgs: [
+          'https://user-images.githubusercontent.com/69447666/107151776-bc079300-69a7-11eb-9de6-8f1f5cc60ec5.jpg',
+          'https://user-images.githubusercontent.com/69447666/107151777-bd38c000-69a7-11eb-93c2-98bc2a19eba5.jpg',
+          'https://user-images.githubusercontent.com/69447666/107151778-be69ed00-69a7-11eb-8fcb-9eea9e3629e4.jpg',
+          'https://user-images.githubusercontent.com/69447666/107151777-bd38c000-69a7-11eb-93c2-98bc2a19eba5.jpg',
+          'https://user-images.githubusercontent.com/69447666/107151776-bc079300-69a7-11eb-9de6-8f1f5cc60ec5.jpg',
+          'https://user-images.githubusercontent.com/69447666/107151778-be69ed00-69a7-11eb-8fcb-9eea9e3629e4.jpg'
+        ],
+        visible: false,
+        index: 0,
+        messages: "Press image"
+      }
+    },
+    methods: {
+      show(index) {
+        this.index = index
+        this.visible = true
+      },
+      isOutside(){
+        if(!this.visible){
+            say(this.messages);
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    margin: 10%;
+  }
+
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 50px;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .pic {
+    cursor: pointer;
+  }
+</style>
